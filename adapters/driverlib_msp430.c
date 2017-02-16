83eb3910454a36e18daacf1027b2e0b809a2028a
Bert Kleewein <bertk@microsoft.com>
2017-02-16 10:55:33 -0800

    library consolidation

---
+++ adapters/driverlib_msp430.c
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#include "cs.c"
+#include "dma.c"
+#include "eusci_a_uart.c"
+#include "gpio.c"
+#include "timer_a.c"
