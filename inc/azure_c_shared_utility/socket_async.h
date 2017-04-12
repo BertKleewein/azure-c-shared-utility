21b864c7eda65462278aab6f76c5d6da6ada74db
Roy Sprowl <roys@jetstreamsoftware.com>
2017-04-12 11:03:15 -0700

    more comments

--- inc/azure_c_shared_utility/socket_async.h
+++ inc/azure_c_shared_utility/socket_async.h
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-/** @file ssl_socket.h
- *	@brief	 Implements socket creation for TLSIO adapters.
+/** @file socket_async.h
+ *	@brief	 Abstracts non-blocking sockets.
  */
 
 #ifndef AZURE_SOCKET_ASYNC_H
